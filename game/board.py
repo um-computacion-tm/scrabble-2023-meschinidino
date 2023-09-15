2e4b163e8719e7e5ff0fd8c0b55f9aaea6d0f1a7
meschinidino <f.meschini@alumno.um.edu.ar>
2023-09-15 18:28:52 -0300

    Fixed repetitive code

--- game/board.py
+++ game/board.py
@@ -4,12 +4,13 @@ COLUMNS = 15
 ROWS = 15
 TRIPLE_WORD_SCORE = ((0, 0), (7, 0), (14, 0), (0, 7), (14, 7), (0, 14), (7, 14), (14, 14))
 DOUBLE_WORD_SCORE = ((1, 1), (2, 2), (3, 3), (4, 4), (1, 13), (2, 12), (3, 11), (4, 10), (13, 1), (12, 2), (11, 3),
-                     (10, 4), (13, 13),(12, 12), (11, 11), (10, 10))
+                     (10, 4), (13, 13), (12, 12), (11, 11), (10, 10))
 TRIPLE_LETTER_SCORE = ((1, 5), (1, 9), (5, 1), (5, 5), (5, 9), (5, 13), (9, 1), (9, 5), (9, 9), (9, 13), (13, 5),
                        (13, 9))
 DOUBLE_LETTER_SCORE = ((0, 3), (0, 11), (2, 6), (2, 8), (3, 0), (3, 7), (3, 14), (6, 2), (6, 6), (6, 8), (6, 12),
-                       (7, 3), (7, 11), (8, 2),(8, 6), (8, 8), (8, 12), (11, 0), (11, 7), (11, 14), (12, 6), (12, 8),
+                       (7, 3), (7, 11), (8, 2), (8, 6), (8, 8), (8, 12), (11, 0), (11, 7), (11, 14), (12, 6), (12, 8),
                        (14, 3), (14, 11))
+
 
 class PlacementOutOfBounds(Exception):
     pass
@@ -24,17 +25,21 @@ class Board:
 
     def add_premium_squares(self):
         for coordinate in TRIPLE_WORD_SCORE:
-            self.grid[coordinate[0]][coordinate[1]].multiplier_type = "word"
-            self.grid[coordinate[0]][coordinate[1]].multiplier = 3
-        for coordinate in TRIPLE_LETTER_SCORE:
-            self.grid[coordinate[0]][coordinate[1]].multiplier_type = "letter"
-            self.grid[coordinate[0]][coordinate[1]].multiplier = 3
+            self.set_square_multiplier(coordinate, "word", 3)
+
         for coordinate in DOUBLE_WORD_SCORE:
-            self.grid[coordinate[0]][coordinate[1]].multiplier_type = "word"
-            self.grid[coordinate[0]][coordinate[1]].multiplier = 2
+            self.set_square_multiplier(coordinate, "word", 2)
+
+        for coordinate in TRIPLE_LETTER_SCORE:
+            self.set_square_multiplier(coordinate, "letter", 3)
+
         for coordinate in DOUBLE_LETTER_SCORE:
-            self.grid[coordinate[0]][coordinate[1]].multiplier_type = "letter"
-            self.grid[coordinate[0]][coordinate[1]].multiplier = 2
+            self.set_square_multiplier(coordinate, "letter", 2)
+
+    def set_square_multiplier(self, coordinate, multiplier_type, multiplier_value):
+        square = self.grid[coordinate[0]][coordinate[1]]
+        square.multiplier_type = multiplier_type
+        square.multiplier = multiplier_value
 
     def place_tile(self, row, column, tile):
         if not (0 <= row < self.rows and 0 <= column < self.columns):
